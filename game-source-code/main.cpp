cb7073a9a377d1dc81e6e03fde3ef3601e6ad2d7
Warwick Brown <warwickb10@gmail.com>
2024-09-09 19:55:27 +0200

    Initial commit

---
+++ game-source-code/main.cpp
@@ -0,0 +1,12 @@
+#include <raylib-cpp.hpp>
+#include <iostream>
+using namespace raylib; // import the raylib-cpp namespace into the global namespace
+
+
+// Predefined raylib-cpp colours can be found here:
+// https://robloach.github.io/raylib-cpp/classraylib_1_1_color.html#a3c177f10d10851fdf20d09fae83c8e19
+
+int main()
+{
+    return 0;
+}
