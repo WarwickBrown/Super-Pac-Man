9d9b8cb71160662e14a7d5f8ea3cdccf9f6ea72f
Warwick Brown <warwickb10@gmail.com>
2024-10-06 17:32:08 +0200

    Doxygen comments for fruit

--- game-source-code/Fruit.cpp
+++ game-source-code/Fruit.cpp
@@ -1,7 +1,20 @@
+/**
+ * @file Fruit.cpp
+ * @brief Implements the Fruit class.
+ */
+
 #include "Fruit.h"
 
+/**
+ * @brief Constructs a Fruit object at the specified position with a default radius of 50.0f.
+ * 
+ * @param x The x-coordinate of the fruit.
+ * @param y The y-coordinate of the fruit.
+ */
 Fruit::Fruit(float x, float y)
     : Collectable(x, y, 50.0f) {}
 
-// Destructor
+/**
+ * @brief Destructor for the Fruit class.
+ */
 Fruit::~Fruit() {}
--- game-source-code/Fruit.h
+++ game-source-code/Fruit.h
@@ -1,14 +1,39 @@
+/**
+ * @file Fruit.h
+ * @brief Defines the Fruit class, derived from the Collectable class, used to represent fruit collectables in the game.
+ */
+
 #ifndef FRUIT_H
 #define FRUIT_H
 
 #include "Collectable.h"
 
+/**
+ * @class Fruit
+ * @brief A class representing a fruit collectable item in the game.
+ * 
+ * The Fruit class inherits from the Collectable class and represents fruit objects
+ * that Pac-Man can collect to earn points or trigger events in the game.
+ */
 class Fruit : public Collectable {
 public:
+    /**
+     * @brief Constructs a Fruit object with a specified position.
+     * 
+     * @param x The x-coordinate of the fruit.
+     * @param y The y-coordinate of the fruit.
+     */
     Fruit(float x, float y);
-    ~Fruit(); // Destructor
+
+    /**
+     * @brief Destructor for the Fruit class.
+     * 
+     * Cleans up any resources associated with the Fruit object.
+     */
+    ~Fruit();
 
 private:
+    // No additional private members or methods for now.
 };
 
 #endif // FRUIT_H
