340201ed64e2bdbeb09d1e95178bcacf71bb837b
Warwick Brown <warwickb10@gmail.com>
2024-10-06 18:04:23 +0200

    Doxygen comments for main

--- game-source-code/main.cpp
+++ game-source-code/main.cpp
@@ -1,18 +1,33 @@
+/**
+ * @file main.cpp
+ * @brief Entry point for the Pac-Man game.
+ * 
+ * This file contains the main function, which initializes and starts the game loop.
+ */
+
 #include <raylib-cpp.hpp>
 #include <iostream>
 #include "Game.h"
 
+/**
+ * @brief The main function that serves as the entry point for the game.
+ * 
+ * The function creates an instance of the Game class, initializes the game, and starts the main game loop.
+ * It continues to run until the player exits the game.
+ * 
+ * @return An integer value indicating the status of the program upon termination (0 = successful).
+ */
 int main()
 {
-    // Create an instance of the Game class
+    // Create an instance of the Game class.
     Game game;
 
-    // Call the initialise method to set up the game objects, load images, and get ready for the game to start
+    // Call the initialise method to set up the game objects, load images, and get ready for the game to start.
     game.initialise();
 
-    // Enter the main game loop, where the game is run until the player exits
+    // Enter the main game loop, where the game is run until the player exits.
     game.run();
 
-    // Return 0 to indicate successful completion of the program
+    // Return 0 to indicate successful completion of the program.
     return 0;
 }
