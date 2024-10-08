81c552bdba6245d31a5b20f2b9500085ee88a5f2
maccelarimatt <mattmaccelari@gmail.com>
2024-10-08 19:33:50 +0200

    minor changes to splash screen

--- game-source-code/Screen.cpp
+++ game-source-code/Screen.cpp
@@ -54,9 +54,9 @@ void Screen::startScreen(const Score& score, int frameNumber) {
     DrawTextureRec(backgroundAnimation[frameNumber], sourceRec, Vector2{0, 0}, RAYWHITE);
 
     // Draw the title "Super Pac-Man" at the top in yellow with a shadow effect.
-     DrawText("Super Pac-Man", 
-            window.GetWidth() / 2 - MeasureText("Super Pac-Man", 70) / 2 + 2,  // Shadow effect offset
-            window.GetHeight() / 4 + 2 + 20, 70, GRAY);  // Shadow color
+    DrawText("Super Pac-Man", 
+         window.GetWidth() / 2 - MeasureText("Super Pac-Man", 70) / 2 + 2,  // Shadow effect offset
+         window.GetHeight() / 4 + 2 + 20, 70, Color{153, 153, 0, 255});  // Darker Yellow color
     DrawText("Super Pac-Man", 
             window.GetWidth() / 2 - MeasureText("Super Pac-Man", 70) / 2, 
             window.GetHeight() / 4 + 20, 70, YELLOW);  // Main color
@@ -64,17 +64,17 @@ void Screen::startScreen(const Score& score, int frameNumber) {
     // Draw instructions and high score with shadows and colors.
     DrawText("Press ENTER to Start", 
             window.GetWidth() / 2 - MeasureText("Press ENTER to Start", 40) / 2 + 2, 
-            window.GetHeight() / 2 - 28 + 2, 40, DARKGREEN);  // Shadow color
+            window.GetHeight() / 2 - 38 + 2, 40, DARKGREEN);  // Shadow color
     DrawText("Press ENTER to Start", 
             window.GetWidth() / 2 - MeasureText("Press ENTER to Start", 40) / 2, 
-            window.GetHeight() / 2 - 28, 40, GREEN);  // Main color
+            window.GetHeight() / 2 - 38, 40, GREEN);  // Main color
 
     DrawText("Press ESC to Exit", 
             window.GetWidth() / 2 - MeasureText("Press ESC to Exit", 30) / 2 + 2, 
-            window.GetHeight() / 2 + 20 + 2, 30, RED);  // Shadow color
+            window.GetHeight() / 2 + 10 + 2, 30, Color{139, 0, 0, 255});  // Shadow color
     DrawText("Press ESC to Exit", 
             window.GetWidth() / 2 - MeasureText("Press ESC to Exit", 30) / 2, 
-            window.GetHeight() / 2 + 20, 30, RED);  // Main color
+            window.GetHeight() / 2 + 10, 30, RED);  // Main color
 
     DrawText("Use the ARROW KEYS to change direction", 
             window.GetWidth() / 2 - MeasureText("Use the ARROW KEYS to change direction", 25) / 2, 
